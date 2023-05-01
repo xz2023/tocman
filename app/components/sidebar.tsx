c5aac829203847a855b4bfc41c2c73876e14f0a2
zuotongxue <xuduoqian@zuotongxue.com>
2023-05-01 21:37:26 +0800

    xu 19

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -4,7 +4,7 @@ import styles from "./home.module.scss";
 
 import { IconButton } from "./button";
 import SettingsIcon from "../icons/settings.svg";
-import GithubIcon from "../icons/github.svg";
+import GithubIcon from "../icons/diqiubucuo2.svg";
 import ChatGptIcon from "../icons/chatgpt.svg";
 import AddIcon from "../icons/add.svg";
 import CloseIcon from "../icons/close.svg";
@@ -96,9 +96,9 @@ export function SideBar(props: { className?: string }) {
       }`}
     >
       <div className={styles["sidebar-header"]}>
-        <div className={styles["sidebar-title"]}>ChatGPT Next</div>
+        <div className={styles["sidebar-title"]}>测试</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          注重逻辑，避免崩溃.
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
