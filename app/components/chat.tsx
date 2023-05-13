a1e882d85768cce70a3f4b109c0d269532fcf34e
zuotongxue <xuduoqian@zuotongxue.com>
2023-05-13 16:02:47 +0800

    xu 182

--- app/components/chat.tsx
+++ app/components/chat.tsx
@@ -94,15 +94,14 @@ function exportMessages(messages: Message[], topic: string) {
         bordered
         text={Locale.Export.Copy}
         onClick={() => copyToClipboard(mdText)}
-      />, 
-      {/*
-      <IconButton   //隐藏按钮：178
-        key="download"
-        icon={<DownloadIcon />}
-        bordered
-        text={Locale.Export.Download}
-        onClick={() => downloadAs(mdText, filename)}
-    />, */}
+      />,
+      // <IconButton   隐藏按钮：178
+      //   key="download"
+      //   icon={<DownloadIcon />}
+      //   bordered
+      //   text={Locale.Export.Download}
+      //   onClick={() => downloadAs(mdText, filename)}
+      // />,
     ],
   });
 }
