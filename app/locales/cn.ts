014b3301b02f875210829101517625348ea46b5f
zuotongxue <xuduoqian@zuotongxue.com>
2023-05-13 21:55:58 +0800

    xu 193

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -132,16 +132,16 @@ const cn = {
       Title: "历史消息长度压缩阈值",
       SubTitle: "当未压缩的历史消息超过该值时，将进行压缩",
     },
-    Token: {
-      Title: "API Key",
-      SubTitle: "使用自己的 Key 可绕过密码访问限制",
-      Placeholder: "OpenAI API Key",
-    },
+    // Token: {
+    //   Title: "API Key",
+    //   SubTitle: "使用自己的 Key 可绕过密码访问限制",
+    //   Placeholder: "OpenAI API Key",
+    // },
 
-    Usage: {
-      Title: "余额查询",
-      SubTitle(used: any, total: any) {
-        return `本月已使用 $${used}，订阅总额 $${total}`;
+    // Usage: {
+    //   Title: "余额查询",
+    //   SubTitle(used: any, total: any) {
+    //     return `本月已使用 $${used}，订阅总额 $${total}`;
       },
       IsChecking: "正在检查…",
       Check: "重新检查",
