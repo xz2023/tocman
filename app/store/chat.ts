d42547185e9a2d1fb41dd49ca51bbe21ca38ca2c
Simon Sinek <132036477+gptman2023@users.noreply.github.com>
2023-05-26 15:08:02 +0000

    xu 125

--- app/store/chat.ts
+++ app/store/chat.ts
@@ -60,7 +60,7 @@ export const BOT_HELLO: Message = createMessage({
   role: "assistant",
   content: "你这样说话可以吗？", //Locale.Store.BotHello,
 });
-export const BOT_HELLO1: Message = createMessage({
+export const BOT_HELLO2: Message = createMessage({
   role: "user",
   content: "说这样就可以2行.",
 });
