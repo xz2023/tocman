5930c855dbb8c71bf8cd881e9d555b0ad467d7c9
Simon Sinek <132036477+gptman2023@users.noreply.github.com>
2023-06-02 12:33:13 +0800

    Update server.ts
    xu 179

--- app/config/server.ts
+++ app/config/server.ts
@@ -1,3 +1,67 @@
+// import md5 from "spark-md5";
+
+// declare global {
+//   namespace NodeJS {
+//     interface ProcessEnv {
+//       OPENAI_API_KEY?: string;
+//       CODE?: string;
+//       PROXY_URL?: string;
+//       VERCEL?: string;
+//     }
+//   }
+// }
+
+// const ACCESS_CODES = (function getAccessCodes(): Set<string> {
+//   const code = process.env.CODE;
+
+//   try {
+//     const codes = (code?.split(",") ?? [])
+//       .filter((v) => !!v)
+//       .map((v) => md5.hash(v.trim()));
+//     return new Set(codes);
+//   } catch (e) {
+//     return new Set();
+//   }
+// })();
+
+// // export const getServerSideConfig = () => {
+// //   if (typeof process === "undefined") {
+// //     throw Error(
+// //       "[Server Config] you are importing a nodejs-only module outside of nodejs",
+// //     );
+// //   }
+
+// //   return {
+// //     apiKey: process.env.OPENAI_API_KEY,
+// //     code: process.env.CODE,
+// //     codes: ACCESS_CODES,
+// //     needCode: ACCESS_CODES.size > 0,
+// //     proxyUrl: process.env.PROXY_URL,
+// //     isVercel: !!process.env.VERCEL,
+// //   };
+// // };
+
+// export const getServerSideConfig = () => {
+//   if (typeof process === "undefined") {
+//     throw Error(
+//       "[Server Config] you are importing a nodejs-only module outside of nodejs",
+//     );
+//   }
+//   //从这里开始
+//   const apikeys = (process.env.OPENAI_API_KEY ?? "").split(",");
+//   const apiKey = apikeys.at(Math.floor(Math.random() * apikeys.length)) ?? "";
+
+//   return {
+//     apiKey, //到这里结束
+//     code: process.env.CODE,
+//     codes: ACCESS_CODES,
+//     needCode: ACCESS_CODES.size > 0,
+//     proxyUrl: process.env.PROXY_URL,
+//     isVercel: !!process.env.VERCEL,
+//     hideUserApiKey: !!process.env.HIDE_USER_API_KEY,
+//     enableGPT4: !process.env.DISABLE_GPT4,
+//   };
+// };
 import md5 from "spark-md5";
 
 declare global {
@@ -5,8 +69,11 @@ declare global {
     interface ProcessEnv {
       OPENAI_API_KEY?: string;
       CODE?: string;
+      BASE_URL?: string;
       PROXY_URL?: string;
       VERCEL?: string;
+      HIDE_USER_API_KEY?: string; // disable user's api key input
+      DISABLE_GPT4?: string; // allow user to use gpt-4 or not
     }
   }
 }
@@ -24,38 +91,24 @@ const ACCESS_CODES = (function getAccessCodes(): Set<string> {
   }
 })();
 
-// export const getServerSideConfig = () => {
-//   if (typeof process === "undefined") {
-//     throw Error(
-//       "[Server Config] you are importing a nodejs-only module outside of nodejs",
-//     );
-//   }
-
-//   return {
-//     apiKey: process.env.OPENAI_API_KEY,
-//     code: process.env.CODE,
-//     codes: ACCESS_CODES,
-//     needCode: ACCESS_CODES.size > 0,
-//     proxyUrl: process.env.PROXY_URL,
-//     isVercel: !!process.env.VERCEL,
-//   };
-// };
-
 export const getServerSideConfig = () => {
   if (typeof process === "undefined") {
     throw Error(
       "[Server Config] you are importing a nodejs-only module outside of nodejs",
     );
   }
-  //从这里开始
-  const apikeys = (process.env.OPENAI_API_KEY ?? "").split(",");
-  const apiKey = apikeys.at(Math.floor(Math.random() * apikeys.length)) ?? "";
+//   //从这里开始
+  const apiKeys = (process.env.OPENAI_API_KEY ?? '').split(',')
+  const apiKey = apikeys.at(Math.floor(Math.random() * apiKeys.length)) ?? ''
 
+//   return {
+//     apiKey, //到这里结束
   return {
     apiKey, //到这里结束
     code: process.env.CODE,
     codes: ACCESS_CODES,
     needCode: ACCESS_CODES.size > 0,
+    baseUrl: process.env.BASE_URL,
     proxyUrl: process.env.PROXY_URL,
     isVercel: !!process.env.VERCEL,
     hideUserApiKey: !!process.env.HIDE_USER_API_KEY,
