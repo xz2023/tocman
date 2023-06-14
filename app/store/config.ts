908268a1fbe6c33eea00cd07015ca1162575d2cf
xz2023 <127741369+xz2023@users.noreply.github.com>
2023-06-14 18:05:17 +0800

    Update config.ts
    xu 614 +3.5 16k

--- app/store/config.ts
+++ app/store/config.ts
@@ -74,6 +74,10 @@ export const ALL_MODELS = [
     available: true,
   },
   {
+    name: "gpt-3.5-turbo-16k",
+    available: true,
+  },
+  {
     name: "gpt-3.5-turbo-0301",
     available: true,
   },
